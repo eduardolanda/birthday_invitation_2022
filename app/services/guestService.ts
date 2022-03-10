e4e8bc35612bd9d296d771a5e1ac55d15b8d282f
“eduardolanda” <eduardo.landa@outlook.com>
2022-03-09 20:45:25 -0800

    add public keys

--- app/services/guestService.ts
+++ app/services/guestService.ts
@@ -1,10 +1,11 @@
 import { GuestInterface } from "~/interfaces";
 import { createClient } from "@supabase/supabase-js";
+const supabaseUrl: string = "https://jhjinyxrpxcdlkstbysf.supabase.co";
+const supabaseKey: string =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpoamlueXhycHhjZGxrc3RieXNmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDY4NzYyNzQsImV4cCI6MTk2MjQ1MjI3NH0.kYWNTJHR3Y39g2v7l47stLIis-bcYTl1ZVZr_7LCgU4";
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 export const getGuest = async (id: string): Promise<GuestInterface | null> => {
-  const supabaseUrl: string = process.env.SUPABASE_API_URL ?? "";
-  const supabaseKey: string = process.env.SUPABASE_KEY ?? "";
-  const supabase = createClient(supabaseUrl, supabaseKey);
   return id
     ? await supabase
         .from<GuestInterface>("guest") // Message maps to the type of the row in your database.
